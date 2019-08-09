c2387f56912cf17b243e0091197762da29d1e993
Cuberob <git@cuberob.com>
2019-08-09 19:23:15 +0200

    Touch Animation

--- lib/fancy_tab.dart
+++ lib/fancy_tab.dart
@@ -1,5 +1,8 @@
 import 'package:fancy_tab/fancy_tab_item.dart';
+import 'package:fancy_tab/tap_ring.dart';
 import 'package:flutter/material.dart';
+
+const Duration ANIM_DURATION = const Duration(milliseconds: 400);
 
 class FancyTab extends StatefulWidget {
   final FancyTabItem item;
@@ -11,13 +14,20 @@ class FancyTab extends StatefulWidget {
   _FancyTabState createState() => _FancyTabState();
 }
 
-class _FancyTabState extends State<FancyTab> {
+class _FancyTabState extends State<FancyTab> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    final titleText = Text(widget.item.title,);
-    final selectedIndicator = Container(width: 4,
+    final titleText = Text(
+      widget.item.title,
+    );
+    final selectedIndicator = Container(
+      width: 4,
       height: 4,
-      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.black),);
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.black,
+      ),
+    );
     final title = Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -38,16 +48,25 @@ class _FancyTabState extends State<FancyTab> {
           children: <Widget>[
             AnimatedPositioned(
               child: title,
-              duration: Duration(milliseconds: 300),
-              top: widget.selected ? 0 : 200,
+              duration: ANIM_DURATION,
+              top: widget.selected ? 0 : 0,
               bottom: widget.selected ? 0 : -200,
             ),
             AnimatedPositioned(
               child: icon,
-              duration: Duration(milliseconds: 300),
+              duration: ANIM_DURATION,
               top: widget.selected ? -200 : 0,
-              bottom: widget.selected ? 200 : 0,
+              bottom: widget.selected ? 0 : 0,
             ),
+            Positioned.fill(
+              child: Center(
+                child: widget.selected
+                    ? TapRing(
+                        color: Colors.blue,
+                      )
+                    : Container(),
+              ),
+            )
           ],
         );
       },
@@ -57,7 +76,7 @@ class _FancyTabState extends State<FancyTab> {
   @override
   void didUpdateWidget(FancyTab oldWidget) {
     if (oldWidget.selected != widget.selected) {
-      if(widget.selected) {
+      if (widget.selected) {
         print("Show pulse animation?");
       }
     }
